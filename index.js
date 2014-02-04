e24e6023f6cbea445f3d316e3f07ff94603408bd
Jay R Bolton <jayrbolton@gmail.com>
2014-02-03 19:13:57 -0800

    test everything!

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var query = require('query')
-var event = require('event')
+var query = require('query-component')
+var event = require('event-component')
 
-var config = {
-	prefix: 'mm-'
-}
+var config = { prefix: 'mm-' }
 
 var states = {}
 
@@ -16,12 +14,18 @@ machina.config = function(settings) {
 	for (var key in settings) config[key] = settings[key]
 }
 
+machina.init = function(el) {
+	el = el || document.body
+	init(states, el)
+}
+
 module.exports = machina
 
 var State = function() {
 	if (!(this instanceof State)) return new State()
-	this._callback = undefined
-	this.attrs = []
+	this._on_enter = function() {}
+	this._on_exit = function() {}
+	this._attrs = []
 	return this
 }
 
@@ -36,20 +40,18 @@ State.prototype.onexit = function(f) {
 }
 
 State.prototype.enter = function(ev) {
-	var n = ev.currentTarget
 	this._on_enter(ev)
-	for (var i = 0; i < this.attrs.length; ++i) {
-		var attr = this.attrs[i]
+	for (var i = 0; i < this._attrs.length; ++i) {
+		var attr = this._attrs[i]
 		attr.node.setAttribute(attr.name, attr.val)
 	}
 	return this
 }
 
 State.prototype.exit = function(ev) {
-	var n = ev.currentTarget
 	this._on_exit(ev)
-	for (var i = 0; i < this.attrs.length; ++i) {
-		var attr = this.attrs[i]
+	for (var i = 0; i < this._attrs.length; ++i) {
+		var attr = this._attrs[i]
 		attr.node.removeAttribute(attr.name)
 	}
 	return this
@@ -57,9 +59,9 @@ State.prototype.exit = function(ev) {
 
 // When the state
 State.prototype.bind_attr = function(node, name, value) {
-	this.attrs.push({
+	this._attrs.push({
 		node: node,
-		name: attr,
+		name: name,
 		val: value
 	})
 	return this
@@ -67,15 +69,16 @@ State.prototype.bind_attr = function(node, name, value) {
 
 // Functional utils
 
-var init = function(states) {
-	init_enters(states)
-	init_exits(states)
-	init_attrs(states)
+var init = function(states, el) {
+	init_enters(states, el)
+	init_exits(states, el)
+	init_attrs(states, el)
 }
 
-var init_enters = function(states) {
+var init_enters = function(states, el) {
 	var attr = config.prefix + 'enter'
-	var enters = query('*[' + attr + ']')
+	var enters = query.all('*[' + attr + ']', el)
+	if (!enters) return;
 	for (var i = 0; i < enters.length; ++i) {
 		var n = enters[i]
 		var val = parse_attr_val(n.getAttribute(attr))
@@ -83,38 +86,45 @@ var init_enters = function(states) {
 		var name = val[1]
 		if (!states[name]) states[name] = new State(name)
 		var state = states[name]
-		event.bind(n, action, state.enter)
+		var fn = function() { state.enter.call(state) }
+		event.bind(n, action, fn)
 		n.removeAttribute(attr)
 	}
 }
 
-var init_exits = function(states) {
+var init_exits = function(states, el) {
 	var attr = config.prefix + 'exit'
-	var exits = query('*[' + attr + ']')
+	var exits = query.all('*[' + attr + ']', el)
+	if (!exits) return;
 	for (var i = 0; i < exits.length; ++i) {
 		var n = exits[i]
 		var val = parse_attr_val(n.getAttribute(attr))
 		var action = val[0]
 		var name = val[1]
 		var state = states[name]
-		if (state) event.bind(n, action, state.exit)
+		if (state) {
+			var fn = function() { state.exit.call(state) }
+			event.bind(n, action, fn)
+		}
 		n.removeAttribute(attr)
 	}
 }
 
-var init_attrs = function(node, states) {
-	stack = [document.body]
+var init_attrs = function(states, el) {
+	var stack = [el]
 	while (stack.length > 0) {
 		var node = stack.pop()
 		var attrs = node.attributes
-		for (var i = 0; i < attrs.length; ++i) {
-			var attr = attrs[i]
-			if (attr.name.indexOf(config.prefix) === 0) {
-				var name_parsed = parse_attr_name(attr.name)
-				var parsed = parse_attr_val(attr.value)
-				var state = parsed[0]
-				var value = parsed[1]
-				if (states[state]) states[state].bind_attr(node, name_parsed, value)
+		if (attrs) {
+			for (var i = 0; i < attrs.length; ++i) {
+				var attr = attrs[i]
+				if (attr.name.indexOf(config.prefix) === 0) {
+					var name_parsed = parse_attr_name(attr.name)
+					var parsed = parse_attr_val(attr.value)
+					var state = parsed[0]
+					var value = parsed[1]
+					if (states[state]) states[state].bind_attr(node, name_parsed, value)
+				}
 			}
 		}
 		var children = node.childNodes
@@ -123,11 +133,9 @@ var init_attrs = function(node, states) {
 }
 
 var parse_attr_val = function(attr_val) {
-	return attr_name.replace(/\s+/g, '').split(':')
+	return attr_val.replace(/\s+/g, '').split(':')
 }
 
 var parse_attr_name = function(attr_name) {
-	return attr_name.replace(/\s+/g,'').(config.prefix,'')
+	return attr_name.replace(/\s+/g,'').replace(config.prefix,'')
 }
-
-init(states)
---
+++ test/index.js
@@ -0,0 +1,79 @@
+var assert = require('assert')
+var domify = require('domify')
+
+var machina = require('../')
+
+describe('machina', function() {
+
+	it ('returns undefined without any machina', function() {
+		assert.equal(machina('lol'), undefined)
+	})
+
+	it ('should initialize states based on enters in the dom', function() {
+		var el = domify("<div><p mm-enter='click:lol'></p></div>")
+		machina.init(el)
+		assert.notEqual(machina('lol'), undefined)
+	})
+
+	it ('should enter a state and set attrs on an event', function() {
+		var el = domify("<div mm-class='lol:clicked'><p mm-enter='click:lol'></p></div>")
+		machina.init(el)
+		el.firstChild.click()
+		assert.equal(el.getAttribute('class'), 'clicked')
+	})
+
+	it ('should enter a state when enter called', function() {
+		var el = domify("<div mm-class='lol:clicked'><p mm-enter='click:lol'></p></div>")
+		machina.init(el)
+		machina('lol').enter()
+		assert.equal(el.getAttribute('class'), 'clicked')
+	})
+
+	it ('should exit a state and remove attrs on an event', function() {
+		var wrapper = domify("<div mm-class='lol:clicked'></div>")
+		var enter = domify("<a mm-enter='click:lol'></a>")
+		var exit = domify("<a mm-exit='click:lol'></a>")
+		wrapper.appendChild(enter)
+		wrapper.appendChild(exit)
+		machina.init(wrapper)
+
+		enter.click()
+		assert.equal(wrapper.getAttribute('class'), 'clicked')
+		exit.click()
+		assert.equal(wrapper.getAttribute('class'), null)
+	})
+
+	it ('should exit a state when exit called', function() {
+		var wrapper = domify("<div mm-class='lol:clicked'></div>")
+		var enter = domify("<a mm-enter='click:lol'></a>")
+		var exit = domify("<a mm-exit='click:lol'></a>")
+		wrapper.appendChild(enter)
+		wrapper.appendChild(exit)
+		machina.init(wrapper)
+
+		machina('lol').enter()
+		assert.equal(wrapper.getAttribute('class'), 'clicked')
+		machina('lol').exit()
+		assert.equal(wrapper.getAttribute('class'), null)
+	})
+
+	it ('runs the callbacks', function() {
+		machina.config({prefix: 'data-machina-'})
+		var el = domify("<div><p --enter='click:lol'></p></div>")
+		machina.init(el)
+		var x = 0
+		machina('lol').onenter(function(e) { ++x })
+		machina('lol').enter()
+		machina('lol').onexit(function(e) { ++x })
+		machina('lol').exit()
+		assert.equal(x, 2)
+	})
+
+	it ('can change the prefix with config', function() {
+		machina.config({prefix: 'data-machina-'})
+		var el = domify("<div><p data-machina-enter='click:lol'></p></div>")
+		machina.init(el)
+		assert.notEqual(machina('lol'), undefined)
+	})
+
+})
